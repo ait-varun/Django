941d4225f2298d2154828df75cb883dd0c5a4153
Varun <aitvarunbhatia@gmail.com>
2024-08-06 16:42:34 +0530

    added newyear route

--- newyear/urls.py
+++ newyear/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-# from hello import views
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("<str:name>/", views.greet, name='greet'),
-
 ]
