e7fbe5ab3bcb03b438a07f7d6164d2dacffc448e
Varun <aitvarunbhatia@gmail.com>
2024-08-06 16:09:10 +0530

    updated html render as response

--- hello/views.py
+++ hello/views.py
@@ -6,4 +6,4 @@ def index(request):
     return render(request, "hello/index.html")
 
 def greet(request, name):
-    return HttpResponse(f"Hello  {name.capitalize()}")
+    return render(request, "hello/greet.html", {'name': name.capitalize()})
