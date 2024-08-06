17ac183f88244809a1e693e20a1a25118c5499d8
Varun <aitvarunbhatia@gmail.com>
2024-08-06 17:18:18 +0530

    added task list

--- newyear/views.py
+++ newyear/views.py
@@ -1,9 +1,12 @@
 import datetime
 from django.shortcuts import render
 
+tasks = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5", "Task 6", "Task 7", "Task 8", "Task 9", "Task 10"]
+
 # Create your views here.
 def index(request):
     now = datetime.datetime.now()
     return render(request, "newyear/index.html",{
       "newyear": now.month == 1 and now.day == 1,
+      "tasks": tasks
     })
