ab4a348f1ceef11ae85ac51dbcb5ecc03ad1bf35
Varun <aitvarunbhatia@gmail.com>
2024-08-07 11:31:43 +0530

    add task form

--- newyear/views.py
+++ newyear/views.py
@@ -1,7 +1,11 @@
 import datetime
+from django import forms
 from django.shortcuts import render
 
 tasks = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5", "Task 6", "Task 7", "Task 8", "Task 9", "Task 10"]
+
+class NewTaskForm(forms.Form):
+  task = forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
@@ -10,3 +14,14 @@ def index(request):
       "newyear": now.month == 1 and now.day == 1,
       "tasks": tasks
     })
+
+def add(request):
+  if request.method == "POST":
+    form = NewTaskForm(request.POST)
+    if form.is_valid():
+      task = form.cleaned_data["task"]
+      tasks.append(task)
+      return render(request, "newyear/index.html",{"tasks": tasks})
+    else:
+      return render(request, "newyear/add.html",{"form": form})
+  return render(request, "newyear/add.html",{"form": NewTaskForm()})
---
+++ taskform/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TaskformConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'taskform'
