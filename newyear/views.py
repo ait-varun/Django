cf7037a39906fa45a077abf2dc05d557f24c65a1
Varun <aitvarunbhatia@gmail.com>
2024-08-07 11:35:46 +0530

    updated task

--- newyear/views.py
+++ newyear/views.py
@@ -2,7 +2,7 @@ import datetime
 from django import forms
 from django.shortcuts import render
 
-tasks = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5", "Task 6", "Task 7", "Task 8", "Task 9", "Task 10"]
+tasks = []
 
 class NewTaskForm(forms.Form):
   task = forms.CharField(label="New Task")
